08623259d10901c0ae6b71eabe2842e83f967f1b
Renan Cavichi <renancavichi@gmail.com>
2025-02-25 17:43:27 -0300

    adiciona propriedades no componente de mensagens

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Header from './src/components/Header';
+import Message from './src/components/Message';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <Message avatar="https://github.com/renancavichi.png">Olá tudo bem?</Message>
+      <Message avatar="https://avatars.githubusercontent.com/u/12842076?v=4">Por aqui beleza!</Message>
+      <Message avatar="https://avatars.githubusercontent.com/u/4061891?v=4">Show!</Message>
       <StatusBar style="auto" />
     </View>
   );
